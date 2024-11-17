5ec2115b06b57a0237fb17c79a8c62a087d5fb97
venkat <venkatmariserla21@gmail.com>
2024-11-17 19:44:57 +0530

    Updates

---
+++ precache-manifest.5a3559c11082038d19381acc87f49362.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f7e78a18b46b6ad44c22756d4cfa408",
+    "url": "/TextUtils_App/index.html"
+  },
+  {
+    "revision": "63de9b44f186d73bdbca",
+    "url": "/TextUtils_App/static/css/main.8f1be39b.chunk.css"
+  },
+  {
+    "revision": "0da3a3a7caac8e2de5c7",
+    "url": "/TextUtils_App/static/js/2.a5b0c296.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/TextUtils_App/static/js/2.a5b0c296.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15c2921800b298b2c621",
+    "url": "/TextUtils_App/static/js/3.65346564.chunk.js"
+  },
+  {
+    "revision": "63de9b44f186d73bdbca",
+    "url": "/TextUtils_App/static/js/main.57b690a4.chunk.js"
+  },
+  {
+    "revision": "d468c08cf52ce17c4a21",
+    "url": "/TextUtils_App/static/js/runtime-main.59171b78.js"
+  }
+]);
